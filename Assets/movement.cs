06afb80baa841c8268169bf18407b321de36c1bc
Brian Hilton <brian_hilton@tamu.edu>
2022-10-11 21:05:55 -0500

    added movement

---
+++ Assets/movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float movementSpeed = 12f;
+    private Vector3 velocity;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 initialPos = new Vector3(0, 0, 0);
+        transform.position = initialPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0f;
+        float y = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
+            y = 1;
+
+        if (Keyboard.current.sKey.isPressed)
+            y = -1;
+
+        if (Keyboard.current.dKey.isPressed)
+            x = 1;
+
+        if (Keyboard.current.aKey.isPressed)
+            x = -1;
+
+        Vector3 moveDirection = (transform.right * x) + (transform.up * y);
+        characterController.Move(moveDirection * movementSpeed * Time.deltaTime);
+
+
+    }
+}
